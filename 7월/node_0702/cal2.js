d83dba898af54676976d09266f465310e243c154
hj <61442652+superRookIt@users.noreply.github.com>
2020-07-06 10:20:38 +0900

    update

---
+++ 7월/node_0702/cal2.js
@@ -0,0 +1,22 @@
+
+
+
+var calc = {};
+
+calc.add = function(a, b) {
+	return a + b;
+};
+
+calc.sub = function(a, b) {
+	return a - b;
+};
+
+calc.mul = function(a, b) {
+	return a * b;
+};
+
+calc.div = function(a, b) {
+	return a / b;
+};
+
+module.exports = calc;
